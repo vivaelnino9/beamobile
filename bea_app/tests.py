c1f31d597f7f4500956f819c51822d7033c04a1c
lucas <mcbride.lucas9@gmail.com>
2017-05-19 13:57:50 -0400

    add email test

--- bea_app/tests.py
+++ bea_app/tests.py
@@ -1,3 +1,11 @@
+from beamobile import settings
+from django.core import mail
 from django.test import TestCase
 
-# Create your tests here.
+class EmailTest(TestCase):
+    def test_send_email(self):
+        mail.send_mail('Subject here', 'Here is the message.',
+            'from@example.com', ['to@example.com'],
+            fail_silently=False)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
