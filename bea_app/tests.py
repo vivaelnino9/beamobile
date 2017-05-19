f4af1100e6f568bb79f80843802d11a62c7ac5bc
lucas <mcbride.lucas9@gmail.com>
2017-05-19 15:25:53 -0400

    add user test

--- bea_app/tests.py
+++ bea_app/tests.py
@@ -1,6 +1,18 @@
-from beamobile import settings
+from django.contrib.auth import authenticate
 from django.core import mail
-from django.test import TestCase
+from django.test import Client, TestCase
+
+from .models import *
+
+class UserTest(TestCase):
+    def test_login(self):
+        self.c = Client()
+        self.user = User.objects.create(username='testuser', password='12345', is_active=True, is_staff=True, is_superuser=True)
+        self.user.set_password('hello')
+        self.user.save()
+        self.user = authenticate(username='testuser', password='hello')
+        login = self.c.login(username='testuser', password='hello')
+        self.assertTrue(login)
 
 class EmailTest(TestCase):
     def test_send_email(self):
